04617a0eef1ba32f909b48016090629a76616442
AfnanLearnInBits <afnanlearninbits@gmail.com>
2026-08-06 00:04:26 +0500

    Made homepage, Editor(Add Text, Font size, color, style, font)

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
 @app.route("/")
-def hello_world():
+def home():
     return render_template('index.html')
+
+@app.route("/editor")
+def editor():
+    return render_template('editor.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/js/editor.js
@@ -0,0 +1,97 @@
+const canvas = new fabric.Canvas('myCanvas',{backgroundColor: '#ffffff'});
+const addTextBtn = document.getElementById("add-text-btn");
+addTextBtn.addEventListener("click", function(){
+    const text = new fabric.IText("New Text",
+        {
+            left:150,
+            top:150,
+            fontSize:32,
+            fill:'#000000',
+            fontFamily:"Arial"
+        }
+    );
+    canvas.add(text);
+    canvas.setActiveObject(text);
+    canvas.renderAll();
+});
+const noSelection = document.getElementById("no-selection");
+const textProperties = document.getElementById("text-properties");
+canvas.on("selection:created", function() {
+    console.log("Selection Created")
+    noSelection.style.display = 'none';
+    textProperties.style.display = 'block';
+});
+canvas.on("selection:cleared", function() {
+    console.log("Selection Cleared")
+    noSelection.style.display = 'block';
+    textProperties.style.display = 'none';
+});
+
+const fontSizeInput = document.getElementById("font-size")
+
+fontSizeInput.addEventListener("input", function(){
+    const selectedObject= canvas.getActiveObject();
+    if (selectedObject) {
+        selectedObject.set("fontSize", Number(fontSizeInput.value));
+        canvas.renderAll();
+    
+    }
+})
+
+const textColorInput = document.getElementById("text-color")
+
+textColorInput.addEventListener("input", function(){
+    const selectedObject= canvas.getActiveObject();
+    if (selectedObject) {
+        selectedObject.set("fill", textColorInput.value);
+        canvas.renderAll();
+    
+    }
+})
+
+const fontFamily = document.getElementById("font-family")
+fontFamily.addEventListener("change", function(){
+    const selectedObject= canvas.getActiveObject();
+    if (selectedObject) {
+        selectedObject.set("fontFamily",fontFamily.value);
+        canvas.renderAll();
+    
+    }
+})
+
+const boldBtn = document.getElementById("bold-btn")
+boldBtn.addEventListener("click", function(){
+    const selectedObject= canvas.getActiveObject();
+    if (selectedObject) {
+       if(selectedObject.fontWeight === "bold") {
+        selectedObject.set("fontWeight", "normal");
+       } else{
+        selectedObject.set("fontWeight", "bold");
+       }
+     canvas.renderAll()
+    }
+})
+const italicBtn = document.getElementById("italic-btn")
+italicBtn.addEventListener("click", function(){
+    const selectedObject= canvas.getActiveObject();
+    if (selectedObject) {
+       if(selectedObject.fontWeight === "italic") {
+        selectedObject.set("fontWeight", "normal");
+       } else{
+        selectedObject.set("fontWeight", "italic");
+       }
+     canvas.renderAll()
+    }
+})
+const underlineBtn = document.getElementById("underline-btn")
+underlineBtn.addEventListener("click", function(){
+    const selectedObject= canvas.getActiveObject();
+    if (selectedObject) {
+       if(selectedObject.underline === true) {
+        selectedObject.set("underline", false);
+       } else{
+        selectedObject.set("underline", true);
+       }
+     canvas.renderAll()
+    }
+})
